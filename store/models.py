34bc0cb690f11229a0f774d5f048f946542ae91c
montgarcia094-beep <montgarcia094@gmail.com>
2026-05-25 11:49:24 -0700

    volvi a limpiar

--- store/models.py
+++ store/models.py
@@ -34,7 +34,7 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.PositiveIntegerField(default=0)
-
+    image = models.ImageField(upload_to='products/', null=True, blank=True)
     owner = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
